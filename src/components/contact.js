a472f997b00cf6fc869742a9b039474072490e4e
Eduardo Torres Davila <etdavila10@gmail.com>
2022-08-05 15:10:18 -0700

    Responsive website. Need Navbar on mobile.

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,9 +3,9 @@ import React from 'react';
 const ContactSection = () => {
   return (
     <div id="contact">
-      <span>torre680@umn.edu</span> 
-      <span>|</span>
-      <span>Office: Vincent Hall 424</span>
+      <span id="email">torre680[at]umn.edu</span> 
+      <span id="sep">|</span>
+      <span id="office">Office: Vincent Hall 424</span>
     </div>
   );
 };
