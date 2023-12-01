21514720f51261a3a57607d899490a65b00f84cd
Eduardo Torres Davila <etdavila10@gmail.com>
2023-11-30 22:10:33 -0600

    Starting fresh with website

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://etdavila10.github.io',
   integrations: [tailwind()]
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      neutral: '#FFFFE8',
+      gray: '#333333',
+      teal: '#008080',
+      olive: '#556B2F',
+      wine: '#880000',
+      black: '#0F1A15',
+      offwhite: '#F5F5DC',
+      darkteal: '#005B5A',
+      darkgray: '#1A2628',
+      coral: '#E2725B',
+    },
+		extend: {
+      fontFamily: {
+        sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+        display: ["Merriweather Sans", ...defaultTheme.fontFamily.sans],
+        mono: ["Source Code Pro", ...defaultTheme.fontFamily.mono],
+      },
+      strokeWidth: {
+        '15': '15px',
+      },
+    },
+	},
+	plugins: [],
+}
