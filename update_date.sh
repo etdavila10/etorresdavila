fc20a651c1fb418816185e28c5988cd56b295491
Eduardo Torres Davila <etdavila10@gmail.com>
2024-03-16 11:11:54 -0500

    Updated script to support multiple OS's

--- update_date.sh
+++ update_date.sh
@@ -8,4 +8,9 @@ file="src/components/Footer.astro"
 echo "Inserting date into: $file"
 echo
 
-sed -i "" "s|<span id=\"git-last-commit-date\">[^<]*</span>|<span id=\"git-last-commit-date\">$git_last_commit_date</span>|g" "$file"
+# Check if the system is macOS
+if [[ $(uname) == "Darwin" ]]; then
+	sed -i "" "s|<span id=\"git-last-commit-date\">[^<]*</span>|<span id=\"git-last-commit-date\">$git_last_commit_date</span>|g" "$file"
+else
+	sed -i "s|<span id=\"git-last-commit-date\">[^<]*</span>|<span id=\"git-last-commit-date\">$git_last_commit_date</span>|g" "$file"
+fi
